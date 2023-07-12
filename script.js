475e2cf3559e2e86edc510bd042f8c213271d429
Kai Zhang <kai.zhang@outlook.com.au>
2023-07-12 17:07:53 +1000

    adds updates to styling

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 document
     .querySelector(".nav-bar__hamburger")
     .addEventListener("click", function () {
-        document.getElementById("mobile-nav").style.display = "flex";
+        document.getElementById("mobileNav").style.display = "flex";
     });
 
 // When inside the mobile-nav, clicking the close button OR any of the links removes the mobile-nav
-document.getElementById("close-btn").addEventListener("click", function () {
-    document.getElementById("mobile-nav").style.display = "none";
+document.getElementById("closeBtn").addEventListener("click", function () {
+    document.getElementById("mobileNav").style.display = "none";
 });
 
 // Select all the links inside #mobile-nav and add a click event listener to each
-document.querySelectorAll("#mobile-nav a").forEach(function (link) {
+document.querySelectorAll("#mobileNav a").forEach(function (link) {
     link.addEventListener("click", function () {
-        document.getElementById("mobile-nav").style.display = "none";
+        document.getElementById("mobileNav").style.display = "none";
     });
 });
